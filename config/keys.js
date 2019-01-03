c64e52129d606e14cc16dfb4370448c19e805137
niman565 <nmanjunath@ucdavis.edu>
2019-01-02 22:42:48 -0800

    back end api almost complete

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://nihal:nihal123@ds227352.mlab.com:27352/devconnector_nihal"
+    "mongodb://nihal:nihal123@ds227352.mlab.com:27352/devconnector_nihal",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -20,11 +26,16 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
-
-app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;
 
